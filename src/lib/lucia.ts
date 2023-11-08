7ba97d9f6854d3c113376350150bb29d7b8803ab
ACAHNN <acahn22@gmail.com>
2023-11-08 15:11:31 -0600

    removing sqlite for prisma

--- src/lib/lucia.ts
+++ src/lib/lucia.ts
@@ -1,22 +1,14 @@
 import { lucia } from "lucia";
-import { betterSqlite3 } from "@lucia-auth/adapter-sqlite";
 import { astro } from "lucia/middleware";
-// import "lucia/polyfill/node";
+import { prisma } from "@lucia-auth/adapter-prisma";
+import { PrismaClient } from "@prisma/client";
 
-import sqlite from "better-sqlite3";
-import fs from "fs";
-
-const db = sqlite(":memory:");
-db.exec(fs.readFileSync("schema.sql", "utf8"));
+const client = new PrismaClient();
 
 export const auth = lucia({
-	adapter: betterSqlite3(db, {
-		user: "user",
-		session: "user_session",
-		key: "user_key"
-	}),
-	middleware: astro(),
 	env: import.meta.env.DEV ? "DEV" : "PROD",
+	middleware: astro(),
+	adapter: prisma(client),
 	getUserAttributes: (data) => {
 		return {
 			username: data.username
