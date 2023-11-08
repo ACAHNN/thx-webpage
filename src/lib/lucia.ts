f614196f1cea9cef30c12e479a5f504e1d4cd360
ACAHNN <acahn22@gmail.com>
2023-11-08 15:42:49 -0600

    trying libsql

--- src/lib/lucia.ts
+++ src/lib/lucia.ts
@@ -1,18 +1,25 @@
 import { lucia } from "lucia";
 import { astro } from "lucia/middleware";
-import { prisma } from "@lucia-auth/adapter-prisma";
+//import { prisma } from "@lucia-auth/adapter-prisma";
 //import { PrismaClient } from "@prisma/client";
-import { PrismaClient } from "@prisma/client/edge";
+//import { PrismaClient } from "@prisma/client/edge";
 
-const client = new PrismaClient();
+//const client = new PrismaClient();
+
+import { libsql } from "@lucia-auth/adapter-sqlite";
+import { createClient } from "@libsql/client";
+
+const db = createClient({
+	url: "file:test/main.db"
+});
 
 export const auth = lucia({
 	env: import.meta.env.DEV ? "DEV" : "PROD",
 	middleware: astro(),
-	adapter: prisma(client, {
-		user: "user", // model User {}
-		key: "key", // model Key {}
-		session: "session" // model Session {}
+	adapter: libsql(db, {
+		user: "user",
+		key: "user_key",
+		session: "user_session"
 	}),
 	getUserAttributes: (data) => {
 		return {
