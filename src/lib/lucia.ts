78182bc82cb1b2c4f5a9eec908524cdb569a7529
ACAHNN <acahn22@gmail.com>
2023-11-12 15:29:04 -0600

    wiring prisma up to mongo atlas

--- src/lib/lucia.ts
+++ src/lib/lucia.ts
@@ -1,25 +1,17 @@
 import { lucia } from "lucia";
 import { astro } from "lucia/middleware";
-//import { prisma } from "@lucia-auth/adapter-prisma";
-//import { PrismaClient } from "@prisma/client";
-//import { PrismaClient } from "@prisma/client/edge";
+import { prisma } from "@lucia-auth/adapter-prisma";
+import { PrismaClient } from "@prisma/client";
 
-//const client = new PrismaClient();
-
-import { libsql } from "@lucia-auth/adapter-sqlite";
-import { createClient } from "@libsql/client";
-
-const db = createClient({
-	url: "file:test/main.db"
-});
+const client = new PrismaClient();
 
 export const auth = lucia({
 	env: import.meta.env.DEV ? "DEV" : "PROD",
 	middleware: astro(),
-	adapter: libsql(db, {
-		user: "user",
-		key: "user_key",
-		session: "user_session"
+	adapter: prisma(client, {
+		user: "user", // model User {}
+		key: "key", // model Key {}
+		session: "session" // model Session {}
 	}),
 	getUserAttributes: (data) => {
 		return {
